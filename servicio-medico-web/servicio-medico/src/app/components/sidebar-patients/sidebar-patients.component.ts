99dd08a8670a91d2b533f92e88cdec6978aab3d3
DavidPerezG <davidperezg72001@outlook.com>
2021-12-21 12:57:54 -0700

    Demo Commit

--- servicio-medico-web/servicio-medico/src/app/components/sidebar-patients/sidebar-patients.component.ts
+++ servicio-medico-web/servicio-medico/src/app/components/sidebar-patients/sidebar-patients.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+
+
 
 @Component({
   selector: 'app-sidebar-patients',
   templateUrl: './sidebar-patients.component.html',
   styleUrls: ['./sidebar-patients.component.css']
 })
-export class SidebarPatientsComponent implements OnInit {
+export class SidebarPatientsComponent {
 
-  constructor() { }
+  title = 'servicio-medico';
 
-  ngOnInit(): void {
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
+
+  constructor(
+    private observer: BreakpointObserver,
+    public router: Router,
+    public authService: AuthService,
+    ) {
   }
+
+  ngAfterViewInit(){
+    this.observer.observe(['(max-width: 800px)']).subscribe((res =>{
+      if(res.matches) {
+        this.sidenav.mode ='over';
+        this.sidenav.close();
+      } else {
+        this.sidenav.mode = 'side';
+        this.sidenav.open();
+      }
+    }))
+  }
+
+
+
+  public closeSidenavIf() {
+    if(window.innerWidth <= 800){
+      console.log(window.innerWidth)
+      this.sidenav.close();
+    } 
+  }
+
+  
 
 }
